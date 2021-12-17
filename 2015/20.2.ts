6308e8c0325147215e0afa14282f134b33b1bebe
Caleb Eggensperger <calebegg@google.com>
2021-12-17 10:50:30 -0500

    Fix 2015/20.2

--- 2015/20.2.ts
+++ 2015/20.2.ts
@@ -1,8 +1,8 @@
 const INPUT = 36000000;
 
 for (let house = 0; ; house++) {
-  let amount = 11;
-  for (let elf = 1; elf <= Math.sqrt(house); elf++) {
+  let amount = 0;
+  for (let elf = 1; elf <= house; elf++) {
     if (house % elf == 0 && house / elf <= 50) {
       amount += 11 * elf;
     }
